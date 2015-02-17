9d8f94273b73246ca0ce42bfd1c4900fc31d13ad
Guillaume Abadie <guillaume.abadie@gmail.com>
2015-02-17 15:50:47 +0100

    Implements the JS assertion

---
+++ servweb/static/js/utils.js
@@ -0,0 +1,17 @@
+
+function assert(condition, message)
+{
+    console.assert(condition, message);
+
+    if (!condition)
+    {
+        message = message || "Assertion failed";
+
+        if (typeof Error !== "undefined")
+        {
+            throw new Error(message);
+        }
+
+        throw message; // Fallback
+    }
+}
