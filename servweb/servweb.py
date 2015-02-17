808dac17d90b4bf4866f932ba6602ee9d938c15d
Guillaume Abadie <guillaume.abadie@gmail.com>
2015-02-17 08:49:07 +0100

    Issue #4: Implements REST callbacks on the web server side

--- servweb/servweb.py
+++ servweb/servweb.py
@@ -1,13 +1,39 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 app.debug = True
+
+
+# ------------------------------------------------------------------------------ INDEX
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
+
+# ------------------------------------------------------------------------------ DATA CHUNK
+
+REST_FORMAT='/api/<task_id>/<user_id>'
+
+@app.route(REST_FORMAT, methods=['GET'])
+def get_data_chunk(task_id, user_id):
+    return jsonify({'result': True})
+
+@app.route(REST_FORMAT, methods=['POST'])
+def write_data_chunk(task_id, user_id):
+    return jsonify({'result': True})
+
+@app.route(REST_FORMAT, methods=['PUT'])
+def append_data_chunk(task_id, user_id):
+    return jsonify({'result': True})
+
+@app.route(REST_FORMAT, methods=['DELETE'])
+def delete_data_chunk(task_id, user_id):
+    return jsonify({'result': True})
+
+
+# ------------------------------------------------------------------------------ MAIN
 
 if __name__ == "__main__":
     app.run()
