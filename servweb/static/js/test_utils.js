4c4e6d3cf4d409e6c65b09adad216af33f7a406b
Guillaume Abadie <guillaume.abadie@gmail.com>
2015-02-18 10:29:59 +0100

    Issues #11: Adds test API's documentation

--- servweb/static/js/test_utils.js
+++ servweb/static/js/test_utils.js
@@ -1,6 +1,11 @@
 
 var test_utils = {};
 
+/*
+ * Runs a test suite <testName>
+ * @param <testFunction>: the test suite's function to call
+ * @param <testName>: the test suite's name
+ */
 test_utils.run = function(testFunction, testName)
 {
     test_utils.testId += 1;
@@ -13,6 +18,47 @@ test_utils.run = function(testFunction, testName)
     testFunction();
 }
 
+/*
+ * Asserts a condition.
+ * @param <cond>: the condition to assert in order to pass the test
+ * @param <name>: the assertion's name
+ */
+test_utils.assert = function(cond, name)
+{
+    if (cond)
+    {
+        test_utils.success(name);
+    }
+    else
+    {
+        test_utils.failure(name);
+    }
+}
+
+/*
+ * Commits a success in the currently running test suite
+ * @param <name>: the succes name
+ */
+test_utils.success = function(name)
+{
+    test_utils.testCount += 1;
+    test_utils.refresh();
+}
+
+/*
+ * Commits a failure in the currently running test suite
+ * @param <name>: the failure name
+ */
+test_utils.failure = function(name)
+{
+    test_utils.testCount += 1;
+    test_utils.testFailed += 1;
+    test_utils.refresh();
+}
+
+/*
+ * Refreshs in the currently running test suite's output line
+ */
 test_utils.refresh = function()
 {
     var domElem = document.getElementById('testId' + test_utils.testId);
@@ -31,31 +77,9 @@ test_utils.refresh = function()
     }
 }
 
-test_utils.success = function(name)
-{
-    test_utils.testCount += 1;
-    test_utils.refresh();
-}
-
-test_utils.failure = function(name)
-{
-    test_utils.testCount += 1;
-    test_utils.testFailed += 1;
-    test_utils.refresh();
-}
-
-test_utils.assert = function(cond, name)
-{
-    if (cond)
-    {
-        test_utils.success(name);
-    }
-    else
-    {
-        test_utils.failure(name);
-    }
-}
-
+/*
+ * Inits the test API
+ */
 test_utils.init = function()
 {
     test_utils.domOutput = document.getElementById('test_output');
