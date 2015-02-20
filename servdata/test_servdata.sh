45d8ad7352df5e19b7ff78eea532a197c35c96d9
Guillaume Abadie <guillaume.abadie@gmail.com>
2015-02-20 15:18:48 +0100

    Issue #11: Fixes a bug in servdata/test_servdata.sh not cleaning all processes

--- servdata/test_servdata.sh
+++ servdata/test_servdata.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-sh db.sh &
-sleep 1
+DB_DIR="test.db"
+
+cd "`dirname $0`/../"
+mkdir $DB_DIR
+mongod --dbpath $DB_DIR --logpath "$DB_DIR/mongod.log" &
 MONGO_PID=$!
 
+sleep 1
+
+cd servdata
 py.test test_servdata.py
 
-kill -2 $MONGO_PID
+kill -9 $MONGO_PID
+
+sleep 1
 
 exit 0
