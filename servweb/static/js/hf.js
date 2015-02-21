972b028c4c92ab814fc8f84b144ce2392578a1ba
Guillaume Abadie <guillaume.abadie@gmail.com>
2015-02-21 10:17:18 +0100

    Issue #11: Implements hf.is_hash()

--- servweb/static/js/hf.js
+++ servweb/static/js/hf.js
@@ -2,6 +2,43 @@
 var hf = {};
 
 /*
+ * Checks if the parameter is a valide hash
+ *
+ * @param <hash>: value to check
+ *
+ * @returns true if this is a valide hash
+ */
+hf.is_hash = function(hash)
+{
+    if (typeof hash != 'string' || hash.length != 64)
+    {
+        return false;
+    }
+
+    for (var i = 0; i < hash.length; i++)
+    {
+        var c = hash.charCodeAt(i);
+
+        if (c >= '0'.charCodeAt(0) && c <= '9'.charCodeAt(0))
+        {
+            continue;
+        }
+        else if (c >= 'a'.charCodeAt(0) && c <= 'f'.charCodeAt(0))
+        {
+            continue;
+        }
+        else if (c >= 'A'.charCodeAt(0) && c <= 'F'.charCodeAt(0))
+        {
+            continue;
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+/*
  * Hash function
  *
  * @param <data>: the data to hash.
@@ -13,8 +50,11 @@ hf.hash = function(data)
     assert(typeof data == 'string');
 
     var bitArray = sjcl.hash.sha256.hash("message");
+    var hash = sjcl.codec.hex.fromBits(bitArray);
 
-    return sjcl.codec.hex.fromBits(bitArray);
+    assert(hf.is_hash(hash));
+
+    return hash;
 }
 
 /*
