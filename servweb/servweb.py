68a82ea8eabefc6b13758a76f97108f5940acd53
vzantedeschi <vzantedeschi@gmail.com>
2015-02-23 14:21:52 +0100

    issue #11 : verify RPC connexion

--- servweb/servweb.py
+++ servweb/servweb.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, jsonify, request
 import xmlrpclib
 import os.path
 import json
+import socket
+import time
 
 app = Flask(__name__)
 app.debug = True
+
 server = xmlrpclib.Server('http://localhost:7080/')
 
 # ------------------------------------------------------------------------------ INDEX
@@ -82,4 +85,15 @@ def get_data_chunk():
 # ------------------------------------------------------------------------------ MAIN
 
 if __name__ == "__main__":
+
+    connected = False
+    #verification RPC connexion
+    while not connected:
+        try:
+            server.read_chunk("")
+            connected = True
+        except socket.error as e:
+            print e
+            time.sleep(1)
+
     app.run()
