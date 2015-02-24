f55a78cf5da923feaf6f8f20367e2d96737fc93d
Guillaume Abadie <guillaume.abadie@gmail.com>
2015-02-24 20:50:18 +0100

    Fixes hf_control

--- servweb/static/js/hf_control.js
+++ servweb/static/js/hf_control.js
@@ -9,13 +9,14 @@ hf_control.hello = function()
 hf_control.onload = function()
 {
     hf_control.show_login_form();
-    hf_control.show_inscription_form();
 }
+
 hf_control.show_login_form = function()
 {
-    hf_ui.load_template("login.html",null,document.getElementById("pageContent"));
+    hf_ui.apply_template("login.html", null, document.getElementById("pageContent"));
 }
+
 hf_control.show_inscription_form = function()
 {
-    hf_ui.load_template("inscription.html",null,document.getElementById("pageContent"));
+    hf_ui.apply_template("inscription.html", null, document.getElementById("pageContent"));
 }
