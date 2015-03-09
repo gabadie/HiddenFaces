08c9a30d32b4533f21e13e84f0bd9290948e10e6
Lamrini El Hariri Khalid <lamrinielharirikhalid@gmail.com>
2015-03-09 16:11:00 +0100

    "unmodifiable destination"

--- servweb/static/js/hf_ui_helper.js
+++ servweb/static/js/hf_ui_helper.js
@@ -78,3 +78,10 @@ Handlebars.registerHelper('hf_comment', function(comment, options) {
     out += comment['content'];
     return out;
 });
+
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b)
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
