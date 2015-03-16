d41f27f49d88ef22a4382e090b194b1de4af20d4
vzantedeschi <vzantedeschi@gmail.com>
2015-03-16 15:34:04 +0100

    issue #45 : test verifing nb of posts in discussion

--- servweb/static/js/test_hf_service.js
+++ servweb/static/js/test_hf_service.js
@@ -823,7 +823,7 @@ test_hf_service.main = function()
     //DISCUSSIONS
     test_utils.run(test_hf_service.create_discussion,'test_hf_service.create_discussion');
     test_utils.run(test_hf_service.add_peers_to_discussion,'test_hf_service.add_peers_to_discussion');
-    test_utils.run(test_hf_service.create_discussion_post,'test_hf_service.create_discussion_post');
+    test_utils.run(test_hf_service.create_discussion_posts,'test_hf_service.create_discussion_posts');
 
     // REGISTRY TESTES
     test_utils.run(test_hf_service.is_valide_chunk,"test_hf_service.is_valide_chunk");
