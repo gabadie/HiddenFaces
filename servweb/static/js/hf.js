336812e132f2532e46a57143dfe8182616fe670c
Guillaume Abadie <guillaume.abadie@gmail.com>
2015-03-23 10:50:49 +0100

    Implements hf.strcmp()

--- servweb/static/js/hf.js
+++ servweb/static/js/hf.js
@@ -422,3 +422,18 @@ hf.linkify = function(inputText)
 hf.capitalize = function(string){
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
+
+hf.strcmp = function(str1, str2)
+{
+    // http://kevin.vanzonneveld.net
+    // +   original by: Waldo Malqui Silva
+    // +      input by: Steve Hilder
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +    revised by: gorthaur
+    // *     example 1: strcmp( 'waldo', 'owald' );
+    // *     returns 1: 1
+    // *     example 2: strcmp( 'owald', 'waldo' );
+    // *     returns 2: -1
+
+    return ((str1 == str2) ? 0 : ((str1 > str2) ? 1 : -1));
+}
